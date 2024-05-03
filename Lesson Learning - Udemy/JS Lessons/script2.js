ec45d18652f544b15825ab197842c4070e2c4204
libbybutler <libbypoulton@hotmail.com>
2024-05-03 15:31:26 +0100

    js

--- Lesson Learning - Udemy/JS Lessons/script2.js
+++ Lesson Learning - Udemy/JS Lessons/script2.js
@@ -1,19 +1,42 @@
-var output = [];
-var count = 1;
+// var output = [];
+// var count = 1;
 
-function fizzBuzz() {
-  console.log(output);
+// function fizzBuzz() {
+//   console.log(output);
 
-  if (count % 3 === 0 && count % 5 === 0) {
-    output.push("Fizzbuzz");
-  } else if (count % 3 === 0) {
-    output.push("fizz");
-  } else if (count % 5 === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(count);
-  }
-  count++;
-}
+//   if (count % 3 === 0 && count % 5 === 0) {
+//     output.push("Fizzbuzz");
+//   } else if (count % 3 === 0) {
+//     output.push("fizz");
+//   } else if (count % 5 === 0) {
+//     output.push("Buzz");
+//   } else {
+//     output.push(count);
+//   }
+//   count++;
+// }
 
 // 3 = fizz, 5= buzz
+
+// ---------------------------------------------------------------------
+
+/* You are going to write a function which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+Important: The output should returned from the function and you do not need alert, prompt or console.log. The output should match the example output exactly, including capitalisation and punctuation.
+
+Example Input
+
+["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+Example Output
+
+Michael is going to buy lunch today! */
+
+function whosPaying(names) {
+  /******Don't change the code above*******/
+  //Write your code here.
+
+  names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+  names.length;
+
+  /******Don't change the code below*******/
+}
