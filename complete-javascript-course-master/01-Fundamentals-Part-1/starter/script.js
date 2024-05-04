1ca469489fb3551da3993f741d635a3ce2998077
libbybutler <libbypoulton@hotmail.com>
2024-05-04 13:38:19 +0100

    new course

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Jonas";
+
+// const birthYear = 1991;
+
+// let age = 21;
+// age = 25;
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
