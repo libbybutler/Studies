4a5f2404bd32731e92c339366160545777d83b65
libbybutler <libbypoulton@hotmail.com>
2024-05-19 15:10:46 +0100

    ternary operator
    learning lesson of other operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -188,19 +188,29 @@ console.log(jonasNew); */
 //     console.log(`not a valid day`);
 // }
 
-var day = prompt(`What day is it?`);
+// var day = prompt(`What day is it?`);
 
-if (day === `monday`) {
-  console.log(`Plan course structure`);
-  console.log(`Go to coding meet up`);
-} else if (day === `tuesday`) {
-  console.log(`prepare theory videos`);
-} else if (day === `wednesday` || day === `thursday`) {
-  console.log(`write code examples`);
-} else if (day === `friday`) {
-  console.log(`record videos`);
-} else if (day === `saturday` || day === "sunday") {
-  console.log(`enjoy the weekend`);
-} else {
-  console.log(`not a valid day`);
-}
+// if (day === `monday`) {
+//   console.log(`Plan course structure`);
+//   console.log(`Go to coding meet up`);
+// } else if (day === `tuesday`) {
+//   console.log(`prepare theory videos`);
+// } else if (day === `wednesday` || day === `thursday`) {
+//   console.log(`write code examples`);
+// } else if (day === `friday`) {
+//   console.log(`record videos`);
+// } else if (day === `saturday` || day === "sunday") {
+//   console.log(`enjoy the weekend`);
+// } else {
+//   console.log(`not a valid day`);
+// }
+
+// ----------------------------
+
+// const age = 23;
+// age >= 18
+//   ? console.log(`i like to drink wine 🍷`)
+//   : console.log(`i like to drink water💦`);
+const age = 23;
+const drink = age >= 18 ? `wine 🍷` : `water💦`;
+console.log(drink);
