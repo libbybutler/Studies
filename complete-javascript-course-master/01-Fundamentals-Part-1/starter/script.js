9fcbcfe5513f519791e0ef0b5cb0663c622e1ab1
libbybutler <libbypoulton@hotmail.com>
2024-05-19 13:46:52 +0100

    js learning
    turning switch statements into conditional statements

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -165,25 +165,42 @@ console.log(jonasNew); */
 //   console.log(`Both win the trophy`);
 // }
 
-const day = `friday`;
-switch (day) {
-  case `monday`:
-    console.log(`Plan course structure`);
-    console.log(`Go to coding meetup`);
-    break;
-  case `tuesday`:
-    console.log(`prepare theory videos`);
-    break;
-  case `wednesday`:
-  case `thursday`:
-    console.log(`write code examples`);
-    break;
-  case `friday`:
-    console.log(`record videos`);
-    break;
-  case `saturday`:
-  case `sunday`:
-    console.log(`enjoy the weekend`);
-  default:
-    console.log(`not a valid day`);
+// const day = `friday`;
+// switch (day) {
+//   case `monday`:
+//     console.log(`Plan course structure`);
+//     console.log(`Go to coding meetup`);
+//     break;
+//   case `tuesday`:
+//     console.log(`prepare theory videos`);
+//     break;
+//   case `wednesday`:
+//   case `thursday`:
+//     console.log(`write code examples`);
+//     break;
+//   case `friday`:
+//     console.log(`record videos`);
+//     break;
+//   case `saturday`:
+//   case `sunday`:
+//     console.log(`enjoy the weekend`);
+//   default:
+//     console.log(`not a valid day`);
+// }
+
+var day = prompt(`What day is it?`);
+
+if (day === `monday`) {
+  console.log(`Plan course structure`);
+  console.log(`Go to coding meet up`);
+} else if (day === `tuesday`) {
+  console.log(`prepare theory videos`);
+} else if (day === `wednesday` || day === `thursday`) {
+  console.log(`write code examples`);
+} else if (day === `friday`) {
+  console.log(`record videos`);
+} else if (day === `saturday` || day === "sunday") {
+  console.log(`enjoy the weekend`);
+} else {
+  console.log(`not a valid day`);
 }
