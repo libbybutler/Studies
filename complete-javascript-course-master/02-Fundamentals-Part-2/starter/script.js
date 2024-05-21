cba20c3e98cbdb27a19a4e76f9682d5dc08aff66
libbybutler <libbypoulton@hotmail.com>
2024-05-21 17:32:40 +0100

    JS fundamentals P2
    Lesson learning

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-`use strict`;
+"use strict";
 
 // let hasDriversLicense = false;
 // const passTest = true;
@@ -9,3 +9,26 @@
 // const interface = `Audio`;
 // const private = 534;
 // const if =23;
+
+// ------------------------------------
+
+// function logger() {
+//   console.log(`my name is Jonas`);
+// }
+
+// logger();
+
+// ----------------------------------------
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
