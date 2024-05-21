4326642fc96be23bc68a390d8a8a85687ae4c271
libbybutler <libbypoulton@hotmail.com>
2024-05-21 17:10:23 +0100

    JS fundamentals p2

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`i can drive :)`);
+
+// const interface = `Audio`;
+// const private = 534;
+// const if =23;
