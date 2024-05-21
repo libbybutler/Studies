bc294c57f3eb062d4b43de9af346e8b5687d3910
libbybutler <libbypoulton@hotmail.com>
2024-05-21 21:19:33 +0100

    js fundamentals p2 lessons

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -59,3 +59,37 @@
 // };
 // console.log(yearsUntilRetirement(1991, `Jonas`));
 // console.log(yearsUntilRetirement(1999, `Libby`));
+
+// --------------------------
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+// -----------------------
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirment = 65 - age;
+
+  if (retirment > 0) {
+    return retirment;
+  } else {
+    return -1;
+  }
+  // return `${firstName} retires in ${retirment} years`;
+};
+
+console.log(yearsUntilRetirement(1999, `Libby`));
