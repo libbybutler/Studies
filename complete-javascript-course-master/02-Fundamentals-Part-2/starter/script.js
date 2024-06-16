38a8ec4e9c453c6cb5aa3d77be92669c6770eb9d
libbybutler <libbypoulton@hotmail.com>
2024-06-16 21:22:46 +0100

    js learning

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -307,3 +307,22 @@
 //     console.log(`You continue Learning!`);
 //   }
 // }
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
