16152f0a7d43b4664563e0b08ec460888804f996
libbybutler <libbypoulton@hotmail.com>
2024-06-23 16:58:56 +0100

    loops in loops

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -347,15 +347,22 @@
 //   console.log(jonas[i], typeof jonas[i]);
 // }
 
-const jonas = [
-  "Jonas",
-  "Schmedtmann",
-  2037 - 1991,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-  true,
-];
+// const jonas = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
 
-for (let i = jonas.length - 1; i >= 0; i--) {
-  console.log(jonas[i]);
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(jonas[i]);
+// }
+
+for (let exercise = 1; exercise <= 4; exercise++) {
+  console.log(`--------- starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`lifting weights repetition ${rep}`);
+  }
 }
