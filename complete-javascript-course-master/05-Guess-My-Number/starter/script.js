bec2a07689bbcee1949973d60659350f456ae6b4
libbybutler <libbypoulton@hotmail.com>
2024-08-31 15:35:59 +0100

    guess number lesson learning

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -8,3 +8,17 @@
 
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(`.secretNumber`).textContent = number;
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(`.message`).textContent = `No Number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `Correct Number!`;
+  }
+});
